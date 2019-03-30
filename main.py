f5c169a910d2798e7e5eb67a421536b251132ea9
Aman Jain <aman.jain@gatech.edu>
2019-03-30 19:15:37 -0400

    Fix merge conflict

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ RANDOM_SIZE = 20
 
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb+srv://admin:aaWyedsDgy03jcLc@cluster0-kwnae.gcp.mongodb.net/markov?retryWrites=true'
+app.config['MONGO_URI'] = 'mongodb+srv://words_app:vQy9e9PUZA6ZWPMm@cluster0-kwnae.gcp.mongodb.net/markov?retryWrites=true'
 mongo = PyMongo(app)
 
 
@@ -32,24 +32,28 @@ def words(word):
     word_regex = re.compile(re.escape(word), re.IGNORECASE)
     word_relation = mongo.db.freqs.find_one({'word': word_regex})
     if not word_relation:
-        rand_relations = mongo.db.freqs.aggregate({'$sample': {'size': RANDOM_SIZE}})
+        rand_relations = mongo.db.freqs.aggregate([{'$sample': {'size': RANDOM_SIZE}}])
         return jsonify([rand_relation['word'] for rand_relation in rand_relations])
     freq_pairs = word_relation['freqs']
     freq_pairs.sort(key=lambda f: -f['freq'])
-    return jsonify(freq_pair['word'] for freq_pair in freq_pairs)
+    return jsonify([freq_pair['word'] for freq_pair in freq_pairs])
 
 @app.route('/starters')
 def starters():
-    rand_words = mongo.db.starters.aggregate({'$sample': {'size': STARTER_SIZE}})
-    return jsonify(word['word'] for word in rand_words)
+    rand_words = mongo.db.starters.aggregate([{'$sample': {'size': 20}}])
+    print([w for w in rand_words])
+    for word in rand_words:
+        print(word)
+    return jsonify([word['word'] for word in rand_words])
 
 @app.route('/<other>')
-def handleIllegalRequest(_):
+def handleIllegalRequest(other):
     return "405: Restricted method"
 
 @app.route('/ping')
 def ping():
-    return 'ping'
+    return mongo.db.name
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == '--update-db':
