f5fe0b27b3e0e7ee56aec07d53578565f42f5e05
Aman Jain <aman.jain@gatech.edu>
2019-03-30 15:45:30 -0400

    changed app.py to main.py

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
 from flask import request
 from flask import Flask
-from flask_pymongo import PyMongo
+#from flask_pymongo import PyMongo
 from flask import jsonify
 
 
 app = Flask(__name__)
-app.config.from_object('configurations.DevelopmentConfig')
-mongo = PyMongo(app)
+#app.config.from_object('configurations.DevelopmentConfig')
+#mongo = PyMongo(app)
 
 
 @app.route('/')
 def landingPage():
     return 'welcome to words words words,\n a shakespeare markov chain api'
 
-@app.route('/getwords/<first>/<second>')
-def getwords(first, second):
-    return jsonify(backendGetWords(first,second))
+@app.route('/getword/<word>')
+def getwords(word):
+    return jsonify(backendGetWords(word))
 
 @app.route('/getstart')
 def getstart():
     return jsonify(backendGetStart())
 
-def backendGetWords(first, second):
+def backendGetWords(word):
     ret = []
-    ret.append(first)
-    ret.append(second)
+    ret.append(word)
+    ret.append("is a good")
+    ret.append("word")
     return ret
 
 def backendGetStart():
@@ -33,7 +34,7 @@ def backendGetStart():
 
 @app.route('/<other>')
 def handleIllegalRequest(other):
-    return "Error: Illegal Request"
+    return "6969"
 
 @app.route('/ping')
 def ping():
