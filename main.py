7cf93c97a88d3a4bc04aa0afefc1856da74d1cb5
Aman Jain <aman.jain@gatech.edu>
2019-03-30 15:56:51 -0400

    made things a bit random

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import request
 from flask import Flask
 #from flask_pymongo import PyMongo
 from flask import jsonify
+import random
 
 
 app = Flask(__name__)
@@ -23,13 +24,18 @@ def getstart():
 
 def backendGetWords(word):
     ret = []
-    ret.append(word)
+    max = random.randint(1,10)
+    for i in range(max):
+        ret.append(word)
     ret.append("is a good")
     ret.append("word")
     return ret
 
 def backendGetStart():
     ret = ["hello", "my", "name"]
+    max = random.randint(1,10)
+    for i in range(max):
+        ret.append("is lkjd")
     return ret;
 
 @app.route('/<other>')
