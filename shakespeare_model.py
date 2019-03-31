2aeee83926167fff2a12e779671bf459d8abc14b
Aman Jain <aman.jain@gatech.edu>
2019-03-31 03:49:44 -0400

    added uri

--- shakespeare_model.py
+++ shakespeare_model.py
@@ -8,6 +8,8 @@ import sys
 from bs4 import BeautifulSoup
 import requests
 
+MONGO_URI = "mongodb://admin:<password>@cluster0-shard-00-00-kwnae.gcp.mongodb.net:27017,cluster0-shard-00-01-kwnae.gcp.mongodb.net:27017,cluster0-shard-00-02-kwnae.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
+
 
 BASE_URL = 'http://shakespeare.mit.edu/index.html'
 POOL_SIZE = 10
